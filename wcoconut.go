5f3b0228f3cd0caccff3e48a3a031b45ddc1c6b9
imedgar <ed.ga.ru90@gmail.com>
2024-09-15 12:43:08 +0300

    add std input

--- wcoconut.go
+++ wcoconut.go
@@ -1,53 +1,98 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
-const invOpt = "invalid option. usage wcoconut [-c -l] file"
+const invOpt = "invalid option. usage wcoconut [-c -l -w -m] file"
 
-var path string
+var (
+	path string
+	data []byte
+)
 
 func main() {
 	args := os.Args[1:]
 	if len(args) < 1 {
 		log.Fatal(invOpt)
 	}
+
 	path = args[0]
 	if len(args) == 2 {
 		path = args[1]
 	}
-	dat, err := os.ReadFile(path)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	switch opt := args[0]; opt {
+	input := readStdin()
+	if input != "" {
+		data = []byte(input)
+		path = ""
+	} else {
+		dat, err := os.ReadFile(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data = dat
+	}
+	count(args[0])
+}
+
+func count(opt string) {
+	switch opt {
 	case "-c":
-		fmt.Println(bytesInFile(dat), path)
+		fmt.Println(bytesInFile(), path)
 	case "-l":
-		fmt.Println(linesInFile(dat), path)
+		fmt.Println(linesInFile(), path)
 	case "-w":
-		fmt.Println(wordsInFile(dat), path)
+		fmt.Println(wordsInFile(), path)
 	case "-m":
-		fmt.Println(bytes.Count(dat, []byte(""))-1, path)
+		fmt.Println(bytes.Count(data, []byte(""))-1, path)
 	default:
-		fmt.Println(linesInFile(dat), wordsInFile(dat), bytesInFile(dat), path)
+		fmt.Println(linesInFile(), wordsInFile(), bytesInFile(), path)
 	}
 }
 
-func bytesInFile(dat []byte) int {
-	return len(dat)
+func bytesInFile() int {
+	return len(data)
 }
 
-func linesInFile(dat []byte) int {
-	return len(strings.Split(string(dat), "\n")) - 1
+func linesInFile() int {
+	return len(strings.Split(string(data), "\n")) - 1
 }
 
-func wordsInFile(dat []byte) int {
-	return len(bytes.Fields(dat))
+func wordsInFile() int {
+	return len(bytes.Fields(data))
+}
+
+func readStdin() string {
+	var input strings.Builder
+	reader := bufio.NewReader(os.Stdin)
+
+	done := make(chan struct{})
+	go func() {
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				if err != io.EOF {
+					log.Fatal(err)
+				}
+				break
+			}
+			input.WriteString(line)
+		}
+		close(done)
+	}()
+
+	select {
+	case <-done:
+	case <-time.After(100 * time.Millisecond):
+	}
+
+	return strings.TrimSpace(input.String())
 }
